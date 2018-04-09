5c38992f7eea4821b3ca63c3a6b7334df4a16b9e
Scorpion <lefte1998di@gmail.com>
2018-04-09 13:03:52 +0300

    first commit

---
+++ push.sh
@@ -0,0 +1,4 @@
+git add README.txt TARS\(Traffic\ Accident\ Recording\ System\).mwb TARS\(Traffic\ Accident\ Recording\ System\).mwb.bak push.sh
+git commit -m "first commit"
+git pull origin master
+git push origin master
